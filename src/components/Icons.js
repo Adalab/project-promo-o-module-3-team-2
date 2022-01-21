2c77b947a3c9609eb64e8d2ee76b4bed87547dad
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-01-21 13:50:37 +0100

    solve bug in icons links

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,7 +2,7 @@ const Icons = (props) => {
   console.log(props);
   return (
     <li className={`card__social--icon social__icon__palette${props.palette} js_icon_border`}>
-      <a className='js_previewPhone' href={props.type === '' ? '#' : `${props.href} ${props.type}`}>
+      <a className='js_previewPhone' href={props.type === '' ? '#' : `${props.href}${props.type}`}>
         <i className={`${props.iconName} icon_image__pallete${props.palette}`}></i>
       </a>
     </li>
